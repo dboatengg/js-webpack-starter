4232e54c4e1ceb98e8d2b4a7e925360203623ba8
dboatengg <dicksonboateng67@gmail.com>
2024-02-16 16:02:09 +0000

    Finish Plugin setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  //Entry point
   entry: {
     bundle: path.resolve(__dirname, './src/index.js'),
   },
+
+  //Output
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  //Plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html',
+      filename: 'index.html',
+    }),
+  ],
 };
