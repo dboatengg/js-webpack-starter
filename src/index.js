fb9bb223de4797ddbbc4b5ce93451e87701d9b1d
dboatengg <dicksonboateng67@gmail.com>
2024-02-24 08:41:20 +0000

    complete images/assets section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles/styles.css';
+// import webpackImg from './images/webpack.jpg';
+import laughingImg from './images/laughing.svg';
 
-console.log('hello, world!');
+const img = document.createElement('img');
+img.src = laughingImg;
+document.getElementById('app').appendChild(img);
